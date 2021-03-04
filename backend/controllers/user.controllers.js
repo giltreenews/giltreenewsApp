5247c5468f800fae6e9ac51c8438f9845554062f
HelmiSassi <c.helmi@live.fr>
2021-03-04 22:34:21 +0100

    [HS] installation de la dependance jwt + création fichier user.controllers.js + crétaion des fonctions pour l'enregistrement et la connexion

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,39 @@
+var model = require('../models/user.models');
+const jwt = require('jsonwebtoken');
+
+var userControllers = function () { }
+
+const accessTokenSecret = 'bzQfJfIfxTEnb3El';
+
+userControllers.register = function (req, res) {
+    const user = req.body;
+
+    model.create(user, (err, result) => {
+        if(err) { 
+            console.log(err)
+        }
+        res.send(result)
+    })
+}
+
+userControllers.login = function (req, res) {
+    const loginInfo = req.body;
+    model.findOne({email: loginInfo.email}, (err, user) => {
+        if(err) {
+            res.status(401).send('wrong email or password');
+            return;
+        }
+        if(!user || user.password !== loginInfo.password){
+            res.status(401).send('wrong email or password');
+            return;
+        }
+
+        const accessToken = jwt.sign({ email: user.email,  role: user.role }, accessTokenSecret);
+        user.password = undefined;
+        res.send({user, accessToken});
+    })
+}
+
+
+
+module.exports = userControllers;
