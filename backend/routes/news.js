f3cacf7d81e68a2ebd9d1f28a3b5a76622d62bf0
alaa-belasker <alaa.ben.asker@gmail.com>
2021-03-05 10:56:12 +0100

    [AB] update routes news

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -4,9 +4,9 @@ const router = express.Router();
 var newsController = require('../controllers/news.controllers');
 
 router.post('/', newsController.show);
-router.post('/add', newsController.save);
-router.put('/edit/:id',newsController.update);
+router.post('/add',[authenticateJwt,roleCheck], newsController.save);
+router.put('/edit/:id',[authenticateJwt,roleCheck] ,newsController.update);
 router.get('/select/:id', newsController.showOne);
-router.delete('/delete/:id', newsController.delete);
+router.delete('/delete/:id',[authenticateJwt,roleCheck], newsController.delete);
 router.post('/recherche',newsController.search);
 module.exports = router;
