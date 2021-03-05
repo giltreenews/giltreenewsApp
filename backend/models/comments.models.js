0cb0acb9982004fab01d064f00c46abc9a74b213
alaa-belasker <alaa.ben.asker@gmail.com>
2021-03-05 10:13:39 +0100

    [AB] creation comments coté back

---
+++ backend/models/comments.models.js
@@ -0,0 +1,12 @@
+var db = require('../db/mongo-connect')();
+var Schema = require('mongoose').Schema;
+
+var comments = Schema({
+    articleId: {type: Schema.Types.ObjectId, ref: 'news'},
+    user: Object,
+    content: String,
+    createdAt: Date,
+    answer : String
+
+});
+module.exports = db.model('comments', comments);
