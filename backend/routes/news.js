0d7c176e8cabf007db7baa126881ba0b63302d63
HelmiSassi <c.helmi@live.fr>
2021-03-05 02:03:24 +0100

    [HS]mettre a jour les routes dans le fichier news.js

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -3,22 +3,10 @@ const router = express.Router();
 
 var newsController = require('../controllers/news.controllers');
 
-router.get('/', newsController.show);
-
-router.get('/one',newsController.showOneAny);
-
-router.get('/recently',newsController.showLimitThree)
-router.get('/flashinfos',newsController.showLimitTwenty)
-
-
+router.post('/', newsController.show);
 router.post('/add', newsController.save);
-
 router.put('/edit/:id',newsController.update);
-
 router.get('/select/:id', newsController.showOne);
-
 router.delete('/delete/:id', newsController.delete);
-
-router.get('/search/:category', newsController.searchByCategory);
 
 module.exports = router;
