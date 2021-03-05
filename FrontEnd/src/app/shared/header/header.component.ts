e520d0b4fee1df0875a36b113a7ba73674c7c12b
alaa-belasker <alaa.ben.asker@gmail.com>
2021-03-05 10:48:42 +0100

    [AB] mettre à jour la page header

--- FrontEnd/src/app/shared/header/header.component.ts
+++ FrontEnd/src/app/shared/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { Router } from '@angular/router';
+import { LocalStorageKeysEnum } from 'src/app/enum/localstorage-keys.enum';
+import { RoleEnum } from 'src/app/enum/role.enum';
+import { UserModel } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+  user: UserModel;
 
-  constructor() { }
+  roleEnum = RoleEnum;
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
-    
+
+
+    this.user = this.userService.getConnectedUser();
   }
 
+  signout(): void {
+    localStorage.removeItem(LocalStorageKeysEnum.USER);
+    localStorage.removeItem(LocalStorageKeysEnum.TOKEN);
+    this.user = this.userService.getConnectedUser();
+    this.router.navigateByUrl('/user/home')
+
+  }
 
 }
+
 
