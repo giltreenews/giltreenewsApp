d4e14ccfea5732cfbf221e300f9dd8a7d0538f59
HelmiSassi <c.helmi@live.fr>
2021-03-05 02:11:18 +0100

    [HS]mise a jours fichier news.service

--- FrontEnd/src/app/services/news.service.ts
+++ FrontEnd/src/app/services/news.service.ts
@@ -16,38 +16,22 @@ export class NewsService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<any> {
-    return this.http.get(baseUrl);
-  }
-  getLastthree(): Observable<any> {
-    return this.http.get(`${baseUrl}/recently`);
-  }
-
-  getLasttwenty(): Observable<any> {
-    return this.http.get(`${baseUrl}/flashinfos`);
-  }
-
-  getLastOne(): Observable<any> {
-    return this.http.get(`${baseUrl}/one`);
-  }
-  getById(id: any): Observable<any> {
-    return this.http.get(`${baseUrl}/select/${id}`);
-  }
-  searchByCategory(category: any): Observable<any> {
-    return this.http.get(`${baseUrl}/search/${category}`);
-  }
-
-  create(data: any): Observable<any> {
-    return this.http.post(`${baseUrl}/add`, data);
-  }
-
-  update(id: any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/edit/${id}`, data);
-  }
-
-  delete(id: any): Observable<any> {
-    return this.http.delete(`${baseUrl}/delete/${id}` );
-  }
+    getAll(filter?): Observable<any> {
+      return this.http.post(baseUrl, filter);
+    }
+    getById(id: any): Observable<any> {
+      return this.http.get(`${baseUrl}/select/${id}`);
+    }
+    create(data: any): Observable<any> {
+      return this.http.post(`${baseUrl}/add`, data);
+    }
+    update(id: any, data: any): Observable<any> {
+      return this.http.put(`${baseUrl}/edit/${id}`, data);
+    }
+    delete(id: any): Observable<any> {
+      return this.http.delete(`${baseUrl}/delete/${id}` );
+    }
+  
 
 
 }
